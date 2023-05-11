52f6902711299cc239977af046da8d4a4451ccf0
Jeffrey <562475805lanu@gmail.com>
2023-05-11 20:05:20 +1000

    implement product update & delete

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const productController = require("../Controller/productController");
+const authController = require("../Controller/authController");
 
 const router = express.Router();
 
-router.route("/").get(productController.getAllProduct).post(productController.createProduct);
+router
+  .route("/")
+  .get(productController.getAllProduct)
+  .post(productController.createProduct)
+  .patch(productController.updateProduct)
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin"),
+    productController.deleteProduct
+  );
 
 module.exports = router;

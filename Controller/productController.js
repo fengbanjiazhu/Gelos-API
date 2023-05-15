dcee5e2936168c451fdc42fc4d5b8239fc018a37
Jeffrey <562475805lanu@gmail.com>
2023-05-15 19:40:42 +1000

    added central controller

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,14 +1,16 @@
 const Product = require("../Model/productModel");
+const centralController = require("./centreController");
 const catchAsync = require("../Utils/catchAsync");
 
-exports.getAllProduct = catchAsync(async (req, res, next) => {
-  const products = await Product.find(req.query);
+// exports.getAllProduct = catchAsync(async (req, res, next) => {
+//   const products = await Product.find(req.query);
 
-  res.status(200).json({
-    status: "success",
-    products,
-  });
-});
+//   res.status(200).json({
+//     status: "success",
+//     products,
+//   });
+// });
+exports.getAllProduct = centralController.getAll(Product);
 
 exports.createProduct = catchAsync(async (req, res, next) => {
   const products = await Product.create(req.body);

e267bce683ac2b02bc93bf21e551f9718d321460
Jeffrey <562475805lanu@gmail.com>
2023-05-16 10:23:28 +1000

    order & product with central controller

--- Controller/orderController.js
+++ Controller/orderController.js
@@ -1,62 +1,8 @@
 const Order = require("../Model/orderModel");
-const catchAsync = require("../Utils/catchAsync");
+const centralController = require("./centreController");
 
-exports.getAllOrder = catchAsync(async (req, res, next) => {
-  console.log(req.query);
-  const orders = await Order.find(req.query);
-
-  res.status(200).json({
-    status: "success",
-    count: orders.length,
-    orders,
-  });
-});
-
-exports.getOrder = catchAsync(async (req, res, next) => {
-  const order = await Order.find({ _id: req.params.id });
-
-  res.status(200).json({
-    status: "success",
-    order,
-  });
-});
-
-exports.createOrder = catchAsync(async (req, res, next) => {
-  const newOrder = await Order.create(req.body);
-
-  res.status(200).json({
-    status: "success",
-    newOrder,
-  });
-});
-
-exports.updateOrder = catchAsync(async (req, res, next) => {
-  let updatedOrder;
-  if (req.query.updateMany === "true") {
-    delete req.query.updateMany;
-    updatedOrder = await Order.updateMany(req.query, { $set: req.body });
-  } else {
-    updatedOrder = await Order.findOneAndUpdate(req.query, { $set: req.body });
-  }
-
-  res.status(200).json({
-    status: "success",
-    updatedOrder,
-  });
-});
-
-exports.deleteOrder = catchAsync(async (req, res, next) => {
-  console.log(req.query, req.body);
-
-  if (req.query.deleteMany === "true") {
-    delete req.query.deleteMany;
-    await Order.deleteMany(req.query);
-  } else {
-    await Order.deleteOne(req.query);
-  }
-
-  res.status(200).json({
-    status: "success",
-    message: "data successful deleted",
-  });
-});
+exports.getAllOrder = centralController.getAll(Order);
+exports.getOrder = centralController.getOne(Order);
+exports.createOrder = centralController.createData(Order);
+exports.updateOrder = centralController.updateData(Order);
+exports.deleteOrder = centralController.deleteData(Order);
--- Controller/productController.js
+++ Controller/productController.js
@@ -1,53 +1,7 @@
 const Product = require("../Model/productModel");
 const centralController = require("./centreController");
-const catchAsync = require("../Utils/catchAsync");
 
-// exports.getAllProduct = catchAsync(async (req, res, next) => {
-//   const products = await Product.find(req.query);
-
-//   res.status(200).json({
-//     status: "success",
-//     products,
-//   });
-// });
 exports.getAllProduct = centralController.getAll(Product);
-
-exports.createProduct = catchAsync(async (req, res, next) => {
-  const products = await Product.create(req.body);
-
-  res.status(200).json({
-    status: "success",
-    products,
-  });
-});
-
-exports.updateProduct = catchAsync(async (req, res, next) => {
-  let updatedProduct;
-  if (req.query.updateMany === "true") {
-    delete req.query.updateMany;
-    updatedProduct = await Product.updateMany(req.query, { $set: req.body });
-  } else {
-    updatedProduct = await Product.findOneAndUpdate(req.query, { $set: req.body });
-  }
-
-  res.status(200).json({
-    status: "success",
-    updatedProduct,
-  });
-});
-
-exports.deleteProduct = catchAsync(async (req, res, next) => {
-  console.log(req.query, req.body);
-
-  if (req.query.deleteMany === "true") {
-    delete req.query.deleteMany;
-    await Product.deleteMany(req.query);
-  } else {
-    await Product.deleteOne(req.query);
-  }
-
-  res.status(200).json({
-    status: "success",
-    message: "data successful deleted",
-  });
-});
+exports.createProduct = centralController.createData(Product);
+exports.updateProduct = centralController.updateData(Product);
+exports.deleteProduct = centralController.deleteData(Product);
